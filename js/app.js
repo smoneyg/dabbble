1b7b86f9989814ac946c22ef8be68b7c8b43d482
Sacha Monneger <smonneger@gmail.com>
2015-04-27 19:03:38 -0400

    create route and partial for popular shots

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 'use_strict';
 
-var app = angular.module("dabbble", ["dabbble.controllers"]);
+var app = angular.module("dabbble", ["dabbble.controllers", 'ngRoute']);
 
 app.constant("myConfig", {
   "access_token": [YOUR_DRIBBBLE_ACCESS_TOKEN]
+});
+
+app.config(function($routeProvider){
+  $routeProvider
+  .when("/:list", {controller:"ShotsListCtrl", templateUrl: "partials/shots_list.html"})
+  .otherwise({redirectTo: "/shots"});
 });
