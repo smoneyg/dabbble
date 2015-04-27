a1d2e166d53378c15b74ccbc823a45c08e8fca98
Sacha Monneger <smonneger@gmail.com>
2015-04-27 16:41:09 -0400

    Define app module and controllers + consume dribbble api to display shots

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
 'use_strict';
+
+var app = angular.module("dabbble", ["dabbble.controllers"]);
+
+app.constant("myConfig", {
+  "access_token": [YOUR_DRIBBBLE_ACCESS_TOKEN]
+});
--- js/controllers.js
+++ js/controllers.js
@@ -1,1 +1,14 @@
 'use_strict';
+
+var controllers = angular.module("dabbble.controllers", []);
+
+controllers.controller("AppCtrl", function($scope){
+  $scope.name = "Module";
+});
+
+controllers.controller("ShotsListCtrl", function($scope, $http, myConfig){
+  $http.get("https://api.dribbble.com/v1/shots?access_token="+myConfig.access_token).then(function(data){
+    $scope.list = data.data;
+    console.log(data);
+  });
+});
